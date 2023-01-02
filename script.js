503d9a2e1bbfe5116c132448e1e08ae9e332ada0
Tiago Costa <tiago.tagcosta@gmail.com>
2023-01-01 21:00:12 -0300

    ADD API_KEY

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let photosArray = [];
 
 // Unsplash API
 let count = 5;
-const apiKey = 'XwsN_YYsFJreW5aPrj1bBpAzPhTBZx4eZJx3YH6TnlM';
+const apiKey = 'UNSPLASH_API_KEY';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
